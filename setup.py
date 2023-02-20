48e3eb7adb4f7cfb760b6434d32f131e73222272
dongjaekim-hail <dongjaekim@dankook.ac.kr>
2023-02-20 19:54:55 +0900

    gymnasium version

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
-setup(name='multiagent',
+setup(name='multiagent-gymnasium',
       version='0.0.1',
-      description='Multi-Agent Goal-Driven Communication Environment',
-      url='https://github.com/openai/multiagent-public',
-      author='Igor Mordatch',
-      author_email='mordatch@openai.com',
+      description='Multi-Agent Goal-Driven Communication Environment... gymnasium version',
+      url='https://github.com/dongjaekim-hail/multiagent-particle-envs-gymnasium',
+      author='Dongjae Kim',
+      author_email='dongjaekim@dankook.ac.kr',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      install_requires=['gym', 'numpy-stl']
+      install_requires=['gymnasium', 'numpy-stl', 'pyglet', 'six']
 )
